97ae1f598da6a3763cc903811f9c15af9b6f2650
綾波あやなみ <2574703489@qq.com>
2026-06-19 16:54:49 +0800

    change assignment3

--- assignment3/迷宫.c
+++ assignment3/迷宫.c
@@ -1,235 +1,174 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct {
-    int r;
-    int c;
-} Point;
-
-typedef struct {
-    Point *data;
-    int front;
-    int rear;
-    int capacity;
-} Queue;
-
-void initQueue(Queue *q, int capacity) {
-    q->data = (Point *)malloc(sizeof(Point) * capacity);
-    q->front = 0;
-    q->rear = 0;
-    q->capacity = capacity;
+#define INF 0x3f3f3f3f
+typedef struct MazeNode *PtrToMazeNode;
+struct MazeNode
+{
+    int X;
+    int Y;
+    int Dir;
+};
+typedef PtrToMazeNode Position;
+ 
+typedef struct SNode *PtrToSNode;
+struct SNode
+{
+    struct MazeNode *Data;
+    int Top;
+    int Capacity;
+};
+typedef PtrToSNode Stack;
+ 
+Stack CreateStack(int MaxSize)
+{
+    Stack S;
+    S = (Stack)malloc(sizeof(struct SNode));
+    S->Data = (struct MazeNode *)malloc(sizeof(struct MazeNode) * MaxSize);
+    S->Top = -1;
+    S->Capacity = MaxSize;
+    return S;
 }
-
-int isEmpty(Queue *q) {
-    return q->front == q->rear;
+ 
+int IsFull(Stack S)
+{
+    return S->Top == S->Capacity - 1;
 }
-
-int enqueue(Queue *q, Point p) {
-    if (q->rear >= q->capacity) {
+ 
+int IsEmpty(Stack S)
+{
+    return S->Top == -1;
+}
+ 
+void Push(Stack S, struct MazeNode X)
+{
+    if (!IsFull(S))
+        S->Data[++S->Top] = X;
+}
+ 
+void Pop(Stack S)
+{
+    if (!IsEmpty(S))
+        S->Top--;
+}
+ 
+Position GetTop(Stack S)
+{
+    if (IsEmpty(S))
+        return NULL;
+    return &S->Data[S->Top];
+}
+ 
+int **CreateMatrix(int Row, int Col)
+{
+    int **A, i;
+    A = (int **)malloc(sizeof(int *) * Row);
+    for (i = 0; i < Row; i++)
+        A[i] = (int *)malloc(sizeof(int) * Col);
+    return A;
+}
+ 
+void FreeMatrix(int **A, int Row)
+{
+    int i;
+    for (i = 0; i < Row; i++)
+        free(A[i]);
+    free(A);
+}
+ 
+int Check(int X, int Y, int Row, int Col, int **Maze, int **Visited)
+{
+    if (X < 0 || X >= Row || Y < 0 || Y >= Col)
         return 0;
-    }
-    q->data[q->rear++] = p;
+    if (Maze[X][Y] != 0 || Visited[X][Y])
+        return 0;
     return 1;
 }
-
-Point dequeue(Queue *q) {
-    return q->data[q->front++];
+ 
+void PrintPath(Stack S)
+{
+    int i;
+    printf("Path length: %d\n", S->Top + 1);
+    for (i = 0; i <= S->Top; i++)
+        printf("(%d,%d)%c", S->Data[i].X, S->Data[i].Y, i == S->Top ? '\n' : ' ');
 }
-
-void freeQueue(Queue *q) {
-    free(q->data);
-}
-
-int **createIntMatrix(int rows, int cols) {
-    int **a = (int **)malloc(sizeof(int *) * rows);
-    for (int i = 0; i < rows; i++) {
-        a[i] = (int *)malloc(sizeof(int) * cols);
+ 
+int SearchPath(int **Maze, int Row, int Col, int Sx, int Sy, int Tx, int Ty)
+{
+    int **Visited, D, Nx, Ny;
+    int Dx[4] = {0, 1, 0, -1};
+    int Dy[4] = {1, 0, -1, 0};
+    Stack S;
+    Position P;
+    struct MazeNode Start, Next;
+    Visited = CreateMatrix(Row, Col);
+    for (Nx = 0; Nx < Row; Nx++)
+        for (Ny = 0; Ny < Col; Ny++)
+            Visited[Nx][Ny] = 0;
+    if (Sx < 0 || Sx >= Row || Sy < 0 || Sy >= Col || Tx < 0 || Tx >= Row || Ty < 0 || Ty >= Col)
+    {
+        FreeMatrix(Visited, Row);
+        return 0;
     }
-    return a;
-}
-
-void freeIntMatrix(int **a, int rows) {
-    for (int i = 0; i < rows; i++) {
-        free(a[i]);
+    if (Maze[Sx][Sy] != 0 || Maze[Tx][Ty] != 0)
+    {
+        FreeMatrix(Visited, Row);
+        return 0;
     }
-    free(a);
-}
-
-int inArea(int r, int c, int rows, int cols) {
-    return r >= 0 && r < rows && c >= 0 && c < cols;
-}
-
-int bfs(int **maze, int rows, int cols, Point start, Point end,
-        int **visited, int **preR, int **preC) {
-    Queue q;
-    initQueue(&q, rows * cols);
-
-    int dr[4] = {1, 0, -1, 0};
-    int dc[4] = {0, 1, 0, -1};
-
-    enqueue(&q, start);
-    visited[start.r][start.c] = 1;
-
-    while (!isEmpty(&q)) {
-        Point cur = dequeue(&q);
-
-        if (cur.r == end.r && cur.c == end.c) {
-            freeQueue(&q);
+    S = CreateStack(Row * Col + 5);
+    Start.X = Sx;
+    Start.Y = Sy;
+    Start.Dir = 0;
+    Push(S, Start);
+    Visited[Sx][Sy] = 1;
+    while (!IsEmpty(S))
+    {
+        P = GetTop(S);
+        if (P->X == Tx && P->Y == Ty)
+        {
+            PrintPath(S);
+            FreeMatrix(Visited, Row);
+            free(S->Data);
+            free(S);
             return 1;
         }
-
-        for (int i = 0; i < 4; i++) {
-            int nr = cur.r + dr[i];
-            int nc = cur.c + dc[i];
-
-            if (inArea(nr, nc, rows, cols)
-                && maze[nr][nc] == 0
-                && visited[nr][nc] == 0) {
-
-                visited[nr][nc] = 1;
-
-                preR[nr][nc] = cur.r;
-                preC[nr][nc] = cur.c;
-
-                Point next;
-                next.r = nr;
-                next.c = nc;
-                enqueue(&q, next);
+        if (P->Dir < 4)
+        {
+            D = P->Dir;
+            P->Dir++;
+            Nx = P->X + Dx[D];
+            Ny = P->Y + Dy[D];
+            if (Check(Nx, Ny, Row, Col, Maze, Visited))
+            {
+                Next.X = Nx;
+                Next.Y = Ny;
+                Next.Dir = 0;
+                Push(S, Next);
+                Visited[Nx][Ny] = 1;
             }
         }
+        else
+        {
+            Pop(S);
+        }
     }
-
-    freeQueue(&q);
+    FreeMatrix(Visited, Row);
+    free(S->Data);
+    free(S);
     return 0;
 }
-
-void printPath(int **maze, int rows, int cols, Point start, Point end,
-               int **preR, int **preC) {
-    Point *path = (Point *)malloc(sizeof(Point) * rows * cols);
-    int pathLen = 0;
-
-    Point cur = end;
-
-    while (!(cur.r == start.r && cur.c == start.c)) {
-        path[pathLen++] = cur;
-
-        Point prev;
-        prev.r = preR[cur.r][cur.c];
-        prev.c = preC[cur.r][cur.c];
-
-        cur = prev;
-    }
-
-    path[pathLen++] = start;
-
-    int **pathFlag = createIntMatrix(rows, cols);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            pathFlag[i][j] = 0;
-        }
-    }
-
-    printf("\n路径坐标如下：\n");
-
-    for (int i = pathLen - 1; i >= 0; i--) {
-        printf("(%d, %d)", path[i].r, path[i].c);
-
-        pathFlag[path[i].r][path[i].c] = 1;
-
-        if (i != 0) {
-            printf(" -> ");
-        }
-    }
-
-    printf("\n\n路径长度：%d 个结点，%d 步\n", pathLen, pathLen - 1);
-
-    printf("\n迷宫路径显示如下：\n");
-    printf("S 表示起点，E 表示终点，* 表示路径，# 表示墙，. 表示可走但未经过\n\n");
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (i == start.r && j == start.c) {
-                printf("S ");
-            } else if (i == end.r && j == end.c) {
-                printf("E ");
-            } else if (maze[i][j] == 1) {
-                printf("# ");
-            } else if (pathFlag[i][j] == 1) {
-                printf("* ");
-            } else {
-                printf(". ");
-            }
-        }
-        printf("\n");
-    }
-
-    free(path);
-    freeIntMatrix(pathFlag, rows);
-}
-
-int main() {
-    int rows, cols;
-
-    printf("请输入迷宫的行数和列数：\n");
-    scanf("%d %d", &rows, &cols);
-
-    Point start, end;
-
-    printf("请输入起点坐标和终点坐标，格式为：start_r start_c end_r end_c\n");
-    scanf("%d %d %d %d", &start.r, &start.c, &end.r, &end.c);
-
-    int **maze = createIntMatrix(rows, cols);
-    int **visited = createIntMatrix(rows, cols);
-    int **preR = createIntMatrix(rows, cols);
-    int **preC = createIntMatrix(rows, cols);
-
-    printf("请输入迷宫数据，0 表示通路，1 表示墙：\n");
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            scanf("%d", &maze[i][j]);
-            visited[i][j] = 0;
-            preR[i][j] = -1;
-            preC[i][j] = -1;
-        }
-    }
-
-    if (!inArea(start.r, start.c, rows, cols) ||
-        !inArea(end.r, end.c, rows, cols)) {
-        printf("起点或终点坐标越界！\n");
-
-        freeIntMatrix(maze, rows);
-        freeIntMatrix(visited, rows);
-        freeIntMatrix(preR, rows);
-        freeIntMatrix(preC, rows);
-
-        return 0;
-    }
-
-    if (maze[start.r][start.c] == 1 || maze[end.r][end.c] == 1) {
-        printf("起点或终点不能是墙！\n");
-
-        freeIntMatrix(maze, rows);
-        freeIntMatrix(visited, rows);
-        freeIntMatrix(preR, rows);
-        freeIntMatrix(preC, rows);
-
-        return 0;
-    }
-
-    int found = bfs(maze, rows, cols, start, end, visited, preR, preC);
-
-    if (found) {
-        printf("\n成功找到一条从起点到终点的路径！\n");
-        printPath(maze, rows, cols, start, end, preR, preC);
-    } else {
-        printf("\n没有找到从起点到终点的路径！\n");
-    }
-
-    freeIntMatrix(maze, rows);
-    freeIntMatrix(visited, rows);
-    freeIntMatrix(preR, rows);
-    freeIntMatrix(preC, rows);
-
+ 
+int main()
+{
+    int Row, Col, Sx, Sy, Tx, Ty, i, j;
+    int **Maze;
+    scanf("%d%d", &Row, &Col);
+    Maze = CreateMatrix(Row, Col);
+    for (i = 0; i < Row; i++)
+        for (j = 0; j < Col; j++)
+            scanf("%d", &Maze[i][j]);
+    scanf("%d%d%d%d", &Sx, &Sy, &Tx, &Ty);
+    if (!SearchPath(Maze, Row, Col, Sx, Sy, Tx, Ty))
+        printf("No path\n");
+    FreeMatrix(Maze, Row);
     return 0;
 }
